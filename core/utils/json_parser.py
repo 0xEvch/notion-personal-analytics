e564a981db9eed92a7703ba443b22919ed600106
0xEvch <quetwmine@icloud.com>
2025-04-08 22:24:20 +0500

    feat: implement full JSON parsing logic with attribute-specific methods in JsonParser

--- core/utils/json_parser.py
+++ core/utils/json_parser.py
@@ -1,12 +1,50 @@
 from core.models.autopilot import Autopilot
-import json
+from datetime import date
+from typing import List
 
 class JsonParser:
-    def parse_notion_data(data: str) -> Autopilot:
-        json_data = json.loads(data)
-        
+    @staticmethod
+    def parse_notion_data(data) -> List[Autopilot]:
         autopilot_objects = []
 
-        properties = json_data.get("results", [])
-        for item in properties.get("properties", []):
-            print(item.get("name", {}).get("title", [{}])[0].get("text", {}).get("content", ""))
+        properties = data.get("results", [])
+        for item in properties:
+            autopilot = Autopilot(
+                name = JsonParser._parse_title(item),
+                activity_type = JsonParser._parse_activity_type(item),
+                date = JsonParser._parse_date(item), 
+                duration_min = JsonParser._parse_duration_min(item),
+                duration_hrs = JsonParser._parse_duration_hrs(item),
+                notes = JsonParser._parse_notes(item),
+            )
+            autopilot_objects.append(autopilot)
+
+    @staticmethod
+    def _parse_title(properties) -> str:
+        return properties["properties"]["Name"]["title"][0]["text"]["content"]
+    
+    @staticmethod
+    def _parse_activity_type(properties) -> str:
+        return properties["properties"]["Activity type"]["select"]["name"]
+    
+    @staticmethod
+    def _parse_date(properties) -> date:
+        date_str = properties["properties"]["Date"]["date"]["start"]
+        return date.fromisoformat(date_str)
+    
+    @staticmethod
+    def _parse_duration_min(properties) -> int:
+        return properties["properties"]["Time (min)"]["number"]
+    
+    @staticmethod
+    def _parse_duration_hrs(properties) -> str:
+        return properties["properties"]["Hrs"]["formula"]["number"]
+    
+    @staticmethod
+    def _parse_notes(properties) -> str:
+        try:
+            rich_text = properties["properties"]["Notes"]["rich_text"]
+            if rich_text:
+                return rich_text["plain_text"]
+        except:
+            return None
