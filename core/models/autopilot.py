4258800d5764fa77f9f611a855b50cd7332c52ce
0xEvch <quetwmine@icloud.com>
2025-04-08 22:25:31 +0500

    refactor: change type of Autopilot.date from datetime to date

--- core/models/autopilot.py
+++ core/models/autopilot.py
@@ -1,13 +1,12 @@
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import date
 from typing import Optional
 
 @dataclass
 class Autopilot:
     name: str
     activity_type: str
-    date: datetime
+    date: date
     duration_min: int
     duration_hrs: float
     notes: Optional[str] = None
-    
