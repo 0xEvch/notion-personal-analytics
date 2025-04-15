15bbc8348e9f1f07adaf3e7d8833779b03a5b755
0xEvch <quetwmine@icloud.com>
2025-04-16 02:37:40 +0500

    fix: add self to get_dataframe method

--- core/utils/pandas_parser.py
+++ core/utils/pandas_parser.py
@@ -2,7 +2,7 @@ import pandas as pd
 from dataclasses import asdict
 
 class PandasParser:
-    def get_dataframe(data):
+    def get_dataframe(self, data):
         df = pd.DataFrame([asdict(item) for item in data])
         df['date'] = pd.to_datetime(df['date'])
         return df
