ddd1e35ec1a090c012361311c5549d0b341749bb
0xEvch <quetwmine@icloud.com>
2025-04-16 02:36:42 +0500

    refactor: remove hardcoded paths in Cache and make it reusable for different data

--- data/notion/cache.py
+++ data/notion/cache.py
@@ -1,53 +1,47 @@
 import pickle
 import os
-from typing import List
-from core.models.autopilot import Autopilot
-
-AUTOPILOT_CACHE = "data/cache/autopilot_cache.pkl"
-SYNC_CACHE = "data/cache/sync_config.pkl"
 
 class Cache:
-    @staticmethod
-    def is_synced(actual_id):
-        last_id = Cache._load_sync_info()
+    def __init__(self, database_path: str, sync_cache_path: str):
+        self.DB_CACHE = database_path
+        self.SYNC_CACHE = sync_cache_path
+
+    def is_synced(self, actual_id: str):
+        last_id = self._load_sync_info()
         if actual_id == last_id:
             return True
         return False
 
-    @staticmethod
-    def save_sync_info(data):
-        os.makedirs(os.path.dirname(SYNC_CACHE), exist_ok=True)
+    def save_sync_info(self, data):
+        os.makedirs(os.path.dirname(self.SYNC_CACHE), exist_ok=True)
         try:
-            with open(SYNC_CACHE, "wb") as f:
+            with open(self.SYNC_CACHE, "wb") as f:
                 pickle.dump(data, f)
         except pickle.PickleError as e:
             print(f"Caching error: {e}")
     
-    @staticmethod
-    def load_cache() -> List[Autopilot]:
-        if os.path.exists(AUTOPILOT_CACHE):
+    def load_cache(self):
+        if os.path.exists(self.DB_CACHE):
             try:
-                with open(AUTOPILOT_CACHE, "rb") as f:
+                with open(self.DB_CACHE, "rb") as f:
                     return pickle.load(f)
             except (pickle.PickleError, EOFError, ValueError) as e:
                 print(f"Loading error: {e}")
                 return None
         return None
-
-    @staticmethod
-    def save_cache(data: List[Autopilot]):
-        os.makedirs(os.path.dirname(AUTOPILOT_CACHE), exist_ok=True)
+    
+    def save_cache(self, data):
+        os.makedirs(os.path.dirname(self.DB_CACHE), exist_ok=True)
         try:
-            with open(AUTOPILOT_CACHE, "wb") as f:
+            with open(self.DB_CACHE, "wb") as f:
                 pickle.dump(data, f)
         except pickle.PickleError as e:
             print(f"Caching error: {e}")
     
-    @staticmethod
-    def _load_sync_info()-> str:
-        if os.path.exists(SYNC_CACHE):
+    def _load_sync_info(self)-> str:
+        if os.path.exists(self.SYNC_CACHE):
             try:
-                with open(SYNC_CACHE, "rb") as f:
+                with open(self.SYNC_CACHE, "rb") as f:
                     return pickle.load(f)
             except (pickle.PickleError, EOFError, ValueError) as e:
                 print(f"Loading error: {e}")
