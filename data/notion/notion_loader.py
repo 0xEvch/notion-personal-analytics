d407043bc55a981e0ba865607cc33e5aa2c99396
0xEvch <quetwmine@icloud.com>
2025-04-15 23:28:21 +0500

    refactor: remove unnecessary module calls to simplify logic

--- data/notion/notion_loader.py
+++ data/notion/notion_loader.py
@@ -1,21 +1,15 @@
 from notion_client import Client
-from config import token, autopilot
-from core.utils.json_parser import JsonParser
 
 class NotionLoader:
     def __init__(self, token, database_id):
         self.client = Client(auth=token)
         self.database = database_id
 
-    def get_database(self):
-        response = self._query_notion()
-        return JsonParser.parse_notion_data(response)
-
     def sync_query(self):
         response = self.client.databases.query(self.database)
         return response["results"][0]["id"]
 
-    def _query_notion(self):
+    def get_database(self):
         results = []
         has_more = True
         next_cursor = None
@@ -35,8 +29,3 @@ class NotionLoader:
                 next_cursor = None
 
         return results
-    
-if __name__ == "__main__":
-    loader = NotionLoader(token, autopilot)
-    print(loader.get_database())
-    print(loader.sync_query())
