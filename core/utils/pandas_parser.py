e445e73fda2486cb019df01957f5e84d5c2f1adb
0xEvch <quetwmine@icloud.com>
2025-04-15 23:27:00 +0500

    refactor: remove static method and rename function to support any database

--- core/utils/pandas_parser.py
+++ core/utils/pandas_parser.py
@@ -2,8 +2,7 @@ import pandas as pd
 from dataclasses import asdict
 
 class PandasParser:
-    @staticmethod
-    def get_autopilot_dataframe(data):
+    def get_dataframe(data):
         df = pd.DataFrame([asdict(item) for item in data])
         df['date'] = pd.to_datetime(df['date'])
         return df
