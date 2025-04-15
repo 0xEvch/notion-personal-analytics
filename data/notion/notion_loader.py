879f1c6a755a935ee4bd21f6b12c1339de12a021
0xEvch <quetwmine@icloud.com>
2025-04-15 21:36:17 +0500

    refactor: make notion_loader reusable for different databases (partial)

--- data/notion/notion_loader.py
+++ data/notion/notion_loader.py
@@ -3,27 +3,28 @@ from config import token, autopilot
 from core.utils.json_parser import JsonParser
 
 class NotionLoader:
-    def __init__(self, token):
+    def __init__(self, token, database_id):
         self.client = Client(auth=token)
+        self.database = database_id
 
     def get_database(self):
-        response = self._query_notion(autopilot)
+        response = self._query_notion()
         return JsonParser.parse_notion_data(response)
 
-    def sync_query(self, database_id=autopilot):
-        response = self.client.databases.query(database_id)
+    def sync_query(self):
+        response = self.client.databases.query(self.database)
         return response["results"][0]["id"]
 
-    def _query_notion(self, database_id):
+    def _query_notion(self):
         results = []
         has_more = True
         next_cursor = None
 
         while has_more:
             if next_cursor:
-                response = self.client.databases.query(database_id, start_cursor=next_cursor)
+                response = self.client.databases.query(self.database, start_cursor=next_cursor)
             else: 
-                response = self.client.databases.query(database_id)
+                response = self.client.databases.query(self.database)
                 
             results.extend(response["results"])
             has_more = response["has_more"]
@@ -36,6 +37,6 @@ class NotionLoader:
         return results
     
 if __name__ == "__main__":
-    loader = NotionLoader(token)
+    loader = NotionLoader(token, autopilot)
     print(loader.get_database())
     print(loader.sync_query())
