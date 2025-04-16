0836976d08c29feebe7f0e9257f929370c6ddafd
0xEvch <quetwmine@icloud.com>
2025-04-17 01:35:16 +0500

    feat: add _add_month to shift formatting logic out of views

--- core/services/base_summary.py
+++ core/services/base_summary.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
+import pandas as pd
 
 class Summary(ABC):
     def __init__(self, data):
@@ -10,8 +11,9 @@ class Summary(ABC):
         for offset in range(months_back):
             month, year = self._get_month_by_offset(offset)
             summary = self._get_month_summary(month, year)
+            summary = self._add_month(summary, month)
             summaries.append(summary)
-        return summaries
+        return pd.concat(summaries, ignore_index=True)
 
     def _get_month_by_offset(self, offset: int):
         month = datetime.now().month - offset
@@ -20,6 +22,11 @@ class Summary(ABC):
             month += 12
             year -= 1
         return month, year
+    
+    def _add_month(self, df, month):
+        months = ["January", "February", "March", "April", "May", "June", "August", "September", "October", "November", "December"]
+        df["Month"] = months[month - 1]
+        return df
     
     @abstractmethod
     def _get_month_summary(self, month: int, year: int):
