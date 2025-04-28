21c0167126b0149ce18485b715a301f760668a91
0xEvch <quetwmine@icloud.com>
2025-04-28 23:54:26 +0500

    feat: add core analyzer module for table analytics
    - Added new directory `core/base` and file `analyzer_core.py` to centralize table processing logic.
    - Implemented `AnalyzerCore` class to handle data loading, parsing, analysis, and visualization in a unified workflow.
    - Designed with polymorphic logic to support processing of multiple tables.
    - This addition simplifies API module by reducing code duplication and providing a convenient interface for table analytics.

---
+++ backend/core/base/analyzer_core.py
@@ -0,0 +1,57 @@
+from data.notion.notion_loader import NotionLoader
+from core.utils.autopilot_json_parser import AutopilotJsonParser
+from data.notion.cache import Cache
+from data.notion.sync_manager import SyncManager
+from core.utils.pandas_parser import PandasParser
+from core.services.activity_summary import ActivitySummary
+from core.views.charts_view import Histogram
+from config import *
+
+class AnalyzerCore():
+    def __init__(self, table):    
+        if table not in self.data_providers:
+            raise ValueError(f"Unknown data type: {table}")
+        self.table = table
+
+        self._setup_mappings()        
+        self._initialize_components()
+
+    def run_analytics(self, months_back):
+        raw_data = self.sync_manager.get_data()
+        data = self.pandas_parser.get_dataframe(raw_data)
+        summary = self.analyzer.get_statistics(data, months_back)
+        chart = self.view.create_chart(summary)
+
+        print(summary)
+
+    def _initialize_components(self):
+        loader = self.data_providers[self.table]()
+        json_parser = self.json_parsers[self.table]()
+        cache = self.caches[self.table]()
+        self.pandas_parser = PandasParser()
+        self.sync_manager = SyncManager(loader, json_parser, cache)
+        self.analyzer = self.analyzers[self.table]()
+        self.view = self.views[self.table]()
+    
+    def _setup_mappings(self):
+        self.data_providers = {
+            "autopilot": lambda: NotionLoader(TOKEN, AUTOPILOT),
+        }
+        self.caches = {
+            "autopilot": lambda: Cache(AUTOPILOT_CACHE, AUTOPILOT_SYNC_CACHE)
+        }
+        self.json_parsers = {
+            "autopilot": lambda: AutopilotJsonParser(),
+            # "new_table": NewTableParser(),
+        }
+        self.analyzers = {
+            "autopilot": lambda: ActivitySummary(),
+            # "new_table": NewTableSummary(),
+        }
+        self.views = {
+            "autopilot": lambda: Histogram(),
+        }
+
+if __name__ == "__main__":
+    test = AnalyzerCore("autopilot")
+    test.run_analytics(3)
